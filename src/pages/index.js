318fb5176f7ff01531b0c11bdb42254fa9edc76f
farbs03 <cgeraldfarber@icloud.com>
2022-03-02 18:13:43 -0500

    css working xD

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
-
 export default function Home() {
   return (
     <div>
-
+        {/* delete everything here, this is just for info about the template */}
+        <div className='h-screen flex flex-col items-center justify-center flex-shrink-0'>
+          <p className='font-bold text-xl'>Next JS + Tailwind CSS Template</p>
+          
+        </div>
     </div>
   )
 }
